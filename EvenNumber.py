af5bc2437ffa23ba82c3b33c01cf4df79a8816c3
Gabriel Rocha <a.rochagabri@gmail.com>
2025-01-13 08:49:35 +0000

    Initial commit

---
+++ EvenNumber.py
@@ -0,0 +1,12 @@
+'''
+Programmer:(C) Gabriel Rocha
+Date......:13/01/2025
+Observation:
+'''
+
+number = int(input("Enter the Number:"))
+
+if number % 2 == 0:
+    print("Number is Even")
+else:
+    print("Number is odd")
